8dbf46aeb4bcf7e4444f1a4c3d24f451ea0b6a31
codefish-y <wei75095@gmail.com>
2023-10-27 18:08:09 +0800

    使用bootstrap美化布局样式

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,33 @@
 import "./App.css";
 import axios from "axios";
-import { useEffect } from "react";
+//引入useEffect和useState钩子函数管理状态和副作用
+import { useEffect, useState } from "react";
 
 function App() {
-  useEffect(()=>{
-    axios.get("http://localhost:3001/posts").then((response) =>{
-        console.log(response);
-    })
-  },[])  
-  return <div className="App"></div>;
+  const [listAllPost, setlistAllPost] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:3001/posts").then((response) => {
+      setlistAllPost(response.data);
+    });
+  }, []);
+  //使用map映射取值
+  return (
+    <div className="App">
+      {listAllPost.map((value, key) => {
+        return (
+        <div className="container">
+            <div class="card mx-auto" style={{width:'500px'}}>
+                <div class="card-body">
+                    <h5 class="card-title">{value.title}</h5>
+                    <p class="card-text">{value.postText}</p>
+                    <h6 class="username">{value.username}</h6>
+                </div>
+            </div>
+        </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import  {createRoot}  from "react-dom/client";
 import App from "./App";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 // for React 17 : ReactDOM.render(<App />, document.getElementById('root'));
 createRoot(document.getElementById("root")).render(<App />);
